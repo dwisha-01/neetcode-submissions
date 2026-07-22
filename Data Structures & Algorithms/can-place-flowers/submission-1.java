4b617ce66be6ce9a358a09749805d945da58feb0
Dwisha Gada <dwishagada@gmail.com>
2026-07-22 08:19:37 +0530

    Add: can-place-flowers - submission-1

---
+++ Data Structures & Algorithms/can-place-flowers/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int n1 = flowerbed.length;
+        for(int i=0;i<n1;i++){
+            if(flowerbed[i]==0 &&(i==0 || flowerbed[i-1]==0)  && (i==n1-1 || flowerbed[i+1]==0)){
+                flowerbed[i] = 1;
+                n--;
+                if(n==0){
+                    return true;
+                }
+            }
+        }
+        return n<=0;
+    }
+}
